b5dcdc4a8492e89855bfe4f4285123c0a4de316b
Lynda Foster <lfost42@wgu.edu>
2022-11-11 06:50:12 -0800

    csv2

--- DataCamp/1-.read-csv-pprint.py
+++ DataCamp/1-.read-csv-pprint.py
@@ -4,7 +4,7 @@ from pprint import pprint
 # Open the csv file in read mode
 with open(path, mode="r", encoding="windows-1252") as csv_file:
     # Open csv_file so that each row is a dictionary
-    reader = csv.reader(csv_file)
+    reader = csv.DictReader(csv_file)
     
     # Print the first row
     row = next(reader)
@@ -13,11 +13,11 @@ with open(path, mode="r", encoding="windows-1252") as csv_file:
 
 
 """ OUTPUT: 
-<class 'list'>
-['Date of Sale (dd/mm/yyyy)',
- 'Address',
- 'Postal Code',
- 'County',
- 'Price (€)',
- 'Description of Property']
+<class 'dict'>
+{'Address': '16 BURLEIGH COURT, BURLINGTON ROAD, DUBLIN 4',
+ 'County': 'Dublin',
+ 'Date of Sale (dd/mm/yyyy)': '03/01/2021',
+ 'Description of Property': 'Second-Hand Dwelling house /Apartment',
+ 'Postal Code': 'Dublin 4',
+ 'Price (€)': '€450,000.00'}
 """
